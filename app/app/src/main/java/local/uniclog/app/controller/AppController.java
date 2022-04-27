ed8e155964e7418f176e08f87405893435e0e853
birewallg <g.sstrav@gmail.com>
2022-04-27 22:20:17 +0900

    add - sqlite rest controller

--- app/app/src/main/java/local/uniclog/app/controller/AppController.java
+++ app/app/src/main/java/local/uniclog/app/controller/AppController.java
@@ -2,6 +2,8 @@ package local.uniclog.app.controller;
 
 import local.uniclog.app.h2.entity.H2DBEntity;
 import local.uniclog.app.h2.service.H2DBService;
+import local.uniclog.app.sqlite.entity.SQLiteDBEntity;
+import local.uniclog.app.sqlite.service.SQLiteDBService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +17,37 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/AccessController")
+@RequestMapping("/api")
 public class AppController {
     private final H2DBService serviceH2;
+    private final SQLiteDBService serviceSQLite;
 
-    @PutMapping("/addH2")
+    @PutMapping("/H2/addH2")
     public ResponseEntity<H2DBEntity> addH2() {
         var entity = serviceH2.build();
         entity = serviceH2.save(entity);
         return ResponseEntity.ok().body(entity);
     }
 
-    @GetMapping("/findAll")
-    public ResponseEntity<List<H2DBEntity>> findAll() {
+    @GetMapping("/H2/findAll")
+    public ResponseEntity<List<H2DBEntity>> findAllH2() {
         var entityList = serviceH2.findAll();
+        return (entityList != null)
+                ? ResponseEntity.ok().body(entityList)
+                : ResponseEntity.notFound().build();
+    }
+
+
+    @PutMapping("/SQLite/add")
+    public ResponseEntity<SQLiteDBEntity> addSQLite() {
+        var entity = serviceSQLite.build();
+        entity = serviceSQLite.save(entity);
+        return ResponseEntity.ok().body(entity);
+    }
+
+    @GetMapping("/SQLite/findAll")
+    public ResponseEntity<List<SQLiteDBEntity>> findAllSQLite() {
+        var entityList = serviceSQLite.findAll();
         return (entityList != null)
                 ? ResponseEntity.ok().body(entityList)
                 : ResponseEntity.notFound().build();
